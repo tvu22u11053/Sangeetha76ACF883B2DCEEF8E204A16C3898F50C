3219bf4e22e903706fd4b4ee2b258271818c7496
tvu22u11053 <144378234+tvu22u11053@users.noreply.github.com>
2023-09-08 10:19:56 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,10 @@
+def fact(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact(n - 1)
+
+
+num = int(input("enter the number:"))
+result = fact(num)
+print("the factorrial value {}is{}".format(num, result))
